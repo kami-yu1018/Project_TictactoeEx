fce503a677b5ad0c25669de0dec1ecd512c32967
4A-02\koyo <a>
2026-09-05 09:53:03 +0900

    ブランチの作成 マウス座標、入力の状態取得 盤面の更新 クリックの長押し防止

--- Game.cpp
+++ Game.cpp
@@ -7,23 +7,65 @@ void Game::Init()
 
 void Game::Update()
 {
+	//　マウスカーソルの座標を取得
+	GetMousePoint(&mouse_pos_x, &mouse_pos_y);
 
+	//　マウスの入力状態を取得
+	mouse_input = GetMouseInput();
+
+	//　入力されていないとき
+	if (key_state == false)
+	{
+		//　左クリックが押されたら
+		if (mouse_input & MOUSE_INPUT_LEFT)
+		{
+			//　カウントを増やす
+			count++;
+		}
+	}
+
+	//　盤面のサイズを制限
+	if (board_size >= 7)
+	{
+		board_size = 7;
+	}
+
+	//　盤面のサイズを更新
+	if (count >= 9 && count < 25)
+	{
+		board_size = 5;
+	}
+	else if (count >= 25 && count < 49)
+	{
+		board_size = 7;
+	}
+
+	//　マウスの長押し入力の防止
+	if (mouse_input == true)
+	{
+		key_state = true;
+	}
+	else
+	{
+		key_state = false;
+	}
 }
 
 void Game::Render()
 {
-	/*for (int j = 0; j < 7; ++j)
+	for (int j = 0; j < board_size; ++j)
 	{
-		for (int i = 0; i < 7; ++i)
+		for (int i = 0; i < board_size; ++i)
 		{
-			int x1 = 50 + (700/7) * (i);
-			int y1 = 100 + (700 / 7) * (j);
-			int x2 = x1 + (700 / 7);
-			int y2 = y1 + (700 / 7);
+			int x1 = 50 + (700 / board_size) * (i);
+			int y1 = 100 + (700 / board_size) * (j);
+			int x2 = x1 + (700 / board_size);
+			int y2 = y1 + (700 / board_size);
 			DrawBox(x1, y1, x2, y2, GetColor(255, 255, 255), FALSE);
 		}
-	}*/
+	}
 
+	DrawFormatString(50, 50, GetColor(255, 255, 255), "Count: %d", count);
 }
 
 void Game::Exit()
--- Game.h
+++ Game.h
@@ -5,6 +5,23 @@
 class Game
 {
 public:
+
+	//　盤面の状態を保持する配列
+	int board[7][7] = { 0 };
+
+	int board_size = 3;
+
+	//　マウス座標
+	int mouse_pos_x;
+	int mouse_pos_y;
+
+	//　マウスの入力状態
+	bool mouse_input = false;
+	bool key_state = false;
+
+	//　記号を置いた数をカウント
+	int count = 0;
+
 	void Init();
 	void Update();
 	void Render();
