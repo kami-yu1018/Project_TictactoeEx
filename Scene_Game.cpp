f441677fc0a49575cc31caaf7dd4839cfca98b9b
4A-02\koyo <a>
2026-09-12 12:04:17 +0900

    盤面を最大7×7から9×9に変更 コメントの追加、変更

--- Scene_Game.cpp
+++ Scene_Game.cpp
@@ -23,21 +23,28 @@ void Scene_Game::Update()
 	}
 
 	//　盤面のサイズを更新
-	//　盤面が埋まったら
+	//　最小サイズ
 	if (count < 9)
 	{
+		//　3×3
 		board_size = 3;
 	}
+	//　盤面が埋まったら
 	else if (count >= 9 && count < 25)
 	{
-		//　盤面のサイズを大きくする
+		//　5×5
 		board_size = 5;
 	}
 	//　また盤面が埋まったら
 	else if (count >= 25 && count < 49)
 	{
-		//　さらに大きくする
+		//　7×7
 		board_size = 7;
+	}
+	else if (count >= 49)
+	{
+		//　9×9
+		board_size = 9;
 	}
 
 	//　左クリックが押されたら
@@ -169,23 +176,28 @@ int Scene_Game::CheckWin()
 	//　勝利に必要な記号の数
 	int win_count = 3;
 
-	//　晩面が3×3の時
+	//　盤面が3×3の時
 	if (board_size == 3)
 	{
 		//　勝利に必要な記号の数は3
 		win_count = 3;
 	}
-	//　晩面が4×4の時
+	//　盤面が4×4の時
 	else if (board_size == 5)
 	{
 		//　4
 		win_count = 4;
 	}
-	//　晩面が5×5の時
-	else
+	//　盤面が5×5の時
+	else if(board_size == 7)
 	{
 		//　5
 		win_count = 5;
+	}
+	//　盤面が9×9の時
+	else if (board_size == 9)
+	{
+		win_count = 6;
 	}
 
 	//　判定する方向
--- Scene_Game.h
+++ Scene_Game.h
@@ -6,8 +6,8 @@ class Scene_Game
 public:
 
 	//　盤面の状態を保持する配列
-	int board[7][7] = { 0 };
-	int draw_player[7][7] = { 0 };
+	int board[9][9] = { 0 };
+	int draw_player[9][9] = { 0 };
 
 	//　ボードのサイズの初期値
 	int board_size = 3;
