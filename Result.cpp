e5942081cb0b5c6559ef53be16bc84225d8ce735
4A-15\koyo <a>
2026-09-19 12:16:06 +0900

    リザルト画面をタイトルみたいな感じに Result.cppのところにコメントで前のコードを残しているけど消しても問題ないはず

--- Result.cpp
+++ Result.cpp
@@ -14,48 +14,128 @@ void Result::Init()
 	nextscene = 0;
 }
 
+//void Result::Update()
+//{
+//	//	マウス座標を取得
+//	int MouseX = GetMouseX();
+//	int MouseY = GetMouseY();
+//
+//	//	左クリックしたとき
+//	if (PushMouseInput(MOUSE_INPUT_LEFT))
+//	{
+//		//	タイトルへ戻る
+//		if (MouseX >= 100 && MouseX <= 700
+//			&& MouseY >= 400 && MouseY <= 450)
+//		{
+//			nextscene = 1;
+//		}
+//
+//		//	リトライ
+//		if (MouseX >= 100 && MouseX <= 700
+//			&& MouseY >= 500 && MouseY <= 550)
+//		{
+//			nextscene = 2;
+//		}
+//	}
+//}
+
 void Result::Update()
 {
-	//	マウス座標を取得
 	int MouseX = GetMouseX();
 	int MouseY = GetMouseY();
 
-	//	左クリックしたとき
+	selectedItem = -1;
+
+	// タイトルへ戻る
+	if (MouseX >= 100 && MouseX <= 700 &&
+		MouseY >= 400 && MouseY <= 450)
+	{
+		selectedItem = 0;
+	}
+
+	// リトライ
+	if (MouseX >= 100 && MouseX <= 700 &&
+		MouseY >= 500 && MouseY <= 550)
+	{
+		selectedItem = 1;
+	}
+
 	if (PushMouseInput(MOUSE_INPUT_LEFT))
 	{
-		//	タイトルへ戻る
-		if (MouseX >= 100 && MouseX <= 700
-			&& MouseY >= 400 && MouseY <= 450)
+		if (selectedItem == 0)
 		{
 			nextscene = 1;
 		}
-
-		//	リトライ
-		if (MouseX >= 100 && MouseX <= 700
-			&& MouseY >= 500 && MouseY <= 550)
+		else if (selectedItem == 1)
 		{
 			nextscene = 2;
 		}
 	}
 }
 
+//void Result::Render(int playresult)
+//{
+//	//	背景画像の描画
+//	DrawGraph(0, 0, backgroundImage, TRUE);
+//	if(playresult==1)	//	勝ちの時
+//	{ 
+//		DrawGraph(150, 100, resultImage[0], TRUE);
+//	}
+//	else if (playresult == 2)	//	負けの時
+//	{
+//		DrawGraph(150, 100, resultImage[1], TRUE);
+//	}
+//
+//	DrawString(100, 400, "タイトルへ戻る", GetColor(255, 255, 255));
+//	DrawString(100, 500, "リトライ", GetColor(255, 255, 255));
+//
+//	DrawFormatString(10, 10, GetColor(255, 255, 255), "%d", nextscene);
+//}
+
 void Result::Render(int playresult)
 {
-	//	背景画像の描画
 	DrawGraph(0, 0, backgroundImage, TRUE);
-	if(playresult==1)	//	勝ちの時
-	{ 
+
+	if (playresult == 1)
+	{
 		DrawGraph(150, 100, resultImage[0], TRUE);
 	}
-	else if (playresult == 2)	//	負けの時
+	else if (playresult == 2)
 	{
 		DrawGraph(150, 100, resultImage[1], TRUE);
 	}
 
-	DrawString(100, 400, "タイトルへ戻る", GetColor(255, 255, 255));
-	DrawString(100, 500, "リトライ", GetColor(255, 255, 255));
+	//---------------------------------
+	// タイトルへ戻る
+	//---------------------------------
+	if (selectedItem == 0)
+	{
+		DrawBox(100, 400,700, 450,GetColor(0, 0, 0),TRUE);
 
-	DrawFormatString(10, 10, GetColor(255, 255, 255), "%d", nextscene);
+		DrawTriangle(70, 410,70, 440,90, 425,GetColor(255, 255, 255),TRUE);
+
+		DrawString(120,410,"タイトルへ戻る",GetColor(255, 255, 255));
+	}
+	else
+	{
+		DrawString(120,410,"タイトルへ戻る",GetColor(0, 0, 0));
+	}
+
+	//---------------------------------
+	// リトライ
+	//---------------------------------
+	if (selectedItem == 1)
+	{
+		DrawBox(100, 500,700, 550,GetColor(0, 0, 0),TRUE);
+
+		DrawTriangle(70, 510,70, 540,90, 525,GetColor(255, 255, 255),TRUE);
+
+		DrawString(120,510,"リトライ",GetColor(255, 255, 255));
+	}
+	else
+	{
+		DrawString(120,510,"リトライ",GetColor(0, 0, 0));
+	}
 }
 
 void Result::Exit()
--- Result.h
+++ Result.h
@@ -8,6 +8,16 @@ private:
 	int backgroundImage; // 背景画像のハンドル
 	int resultImage[2];	// 勝敗画像のハンドル
 
+	int selectedItem = -1;
+
+	static constexpr int MENU_LEFT = 100;
+	static constexpr int MENU_RIGHT = 700;
+	static constexpr int MENU1_TOP = 400;
+	static constexpr int MENU1_BOTTOM = 450;
+	static constexpr int MENU2_TOP = 500;
+	static constexpr int MENU2_BOTTOM = 550;
+
+
 public:
 	int nextscene;	//	次のシーンを示す変数（1:タイトル画面、2:ゲーム画面）
 
